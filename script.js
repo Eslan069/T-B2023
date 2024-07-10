774d2df63d85cf1299a7254b47f1f25915256d6a
Elias Abreu <155367280+Eslan069@users.noreply.github.com>
2024-07-10 10:46:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+/* Ação de eventos sobre o botão menu da tag nav (mobile). */
+/* Parâmetros: Nome do evento, função. */
+openMenu.addEventListener('click', () => {
+
+	/* Sobrepõe a propriedade display: none aplicada no
+	   CSS por display: flex que o torna visível. */ 
+	menu.style.display = "flex"
+
+	/* Captura o tamanho do menu nav e aplica na posição. */
+	menu.style.right = (menu.offsetWidth * -1) + 'px'
+
+	/* Após 10 milésimos de segundo, adiciona o atributo style, */
+	/* e adiciona as propriedades CSS.*/
+	setTimeout(()=> {
+		menu.style.opacity = '1'
+		menu.style.right = "0"
+		openMenu.style.display = 'none'
+	}, 10);
+})
+
+/* Ação de eventos sobre o botão X da tag nav (mobile). */
+/* Parâmetros: Nome do evento, função. */
+closeMenu.addEventListener('click', () => {
+	menu.style.opacity = '0'
+	menu.style.right = (menu.offsetWidth * -1) + 'px'
+
+	/* Torna visível o botão que apresenta o menu nav. */
+	/* openMenu.style.display = 'block'*/
+	
+	/* Após 200 milésimos de 1 segundo, remove o atributo style. */
+	setTimeout(()=> {
+		menu.removeAttribute('style')
+		openMenu.removeAttribute('style')
+	}, 200);
+})
+
+const list = document.querySelectorAll(".list");
+function activelink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("click", activelink));
